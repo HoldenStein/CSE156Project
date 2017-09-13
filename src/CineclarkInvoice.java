5eef7cddb119bd7a6498a4c2c96068bea5fd9fce
Holden Steinhauser <holden.steinhauser@huskers.unl.edu>
2017-09-13 15:31:10 -0500

    Created Scanner and file variables.
    Used try catch block for to check for filenotfoundexception.
    
    Finally looped through each of the files and printed out contents

--- src/CineclarkInvoice.java
+++ src/CineclarkInvoice.java
@@ -1,9 +1,47 @@
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+
+
 public class CineclarkInvoice {
 
-    public static void main(String args[]) {
-        Person person1 = new Person("A100", "Holden Steinhauser", "2232 Cochin St");
 
-        System.out.println(person1.getPerson());
+
+    public static void main(String args[]) {
+
+        Scanner personScanner = null;
+        Scanner productScanner = null;
+        Scanner customerScanner = null;
+        String personFile = "data/Persons.dat";
+        String productFile = "data/Products.dat";
+        String customerFile = "data/Customers.dat";
+        try {
+            personScanner = new Scanner(new File(personFile));
+            productScanner = new Scanner(new File(productFile));
+            customerScanner = new Scanner(new File(customerFile));
+
+        } catch (FileNotFoundException e ) {
+            e.printStackTrace();
+        }
+
+        while(personScanner.hasNext()) {
+            System.out.println(personScanner.nextLine());
+        }
+
+        System.out.println();
+
+        while(productScanner.hasNext()) {
+            System.out.println(productScanner.nextLine());
+        }
+
+        System.out.println();
+
+        while(customerScanner.hasNext()) {
+            System.out.println(customerScanner.nextLine());
+        }
+
     }
 
 }
