58e04aaf42cb85cb1cc1f61f2c7ccc50e664886b
Holden Steinhauser <holden.steinhauser@huskers.unl.edu>
2017-09-13 15:18:00 -0500

    Defined Person class, created an instance in main to test

--- src/CineclarkInvoice.java
+++ src/CineclarkInvoice.java
@@ -1,7 +1,9 @@
 public class CineclarkInvoice {
 
     public static void main(String args[]) {
+        Person person1 = new Person("A100", "Holden Steinhauser", "2232 Cochin St");
 
+        System.out.println(person1.getPerson());
     }
 
 }
