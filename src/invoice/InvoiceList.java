81f4c955e203fe173384bebfdc4cd6525530cabe
MrKp91 <khangpham402@gmail.com>
2017-11-27 00:10:51 -0600

    Add files via upload
    Added removeNode in InvoiceList

--- src/invoice/InvoiceList.java
+++ src/invoice/InvoiceList.java
@@ -29,7 +29,6 @@ public class InvoiceList implements Iterable<Invoice> {
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
 			if (current == null) {
 				return false;
 			} else {
@@ -39,13 +38,14 @@ public class InvoiceList implements Iterable<Invoice> {
 
 		@Override
 		public Invoice next() {
-			// TODO Auto-generated method stub
 			Invoice invoice = current.getInvoice();
 			current = current.getNext();
 			return invoice;
 		}
 
 	}
+	
+	
 
 	public InvoiceNode<Invoice> getNodeAtIndex(int index) {
 
@@ -73,9 +73,9 @@ public class InvoiceList implements Iterable<Invoice> {
 	// Comparing invoiceTotal(TotalSubTotal) from highest to lowest
 
 	public void add(Invoice invoice) {
-		
+
 		InvoiceNode<Invoice> newInvoiceNode = new InvoiceNode<Invoice>(invoice);
-		
+
 		if (start == null) {
 			start = newInvoiceNode;
 			newInvoiceNode.setNext(null);
@@ -103,10 +103,10 @@ public class InvoiceList implements Iterable<Invoice> {
 				start.setNext(null);
 				start = newInvoiceNode;
 				size++;
-				
+
 			}
 			// Has more than two nodes
-		} else if (size > 1) {			
+		} else if (size > 1) {
 
 			// case 1: new node is larger than start (first on the list)
 			if (getComp().compare(invoice, start.getInvoice()) > 0) {
@@ -117,46 +117,85 @@ public class InvoiceList implements Iterable<Invoice> {
 				start = newInvoiceNode;
 
 				size++;
-							
+
 				// case 2: new node is larger than neighbor of start node, new node in between
 			} else if (getComp().compare(invoice, start.getNext().getInvoice()) > 0) {
-				
+
 				InvoiceNode<Invoice> startNeighbor = new InvoiceNode<Invoice>(start.getNext().getInvoice());
-								
+
 				start.setNext(newInvoiceNode);
 				newInvoiceNode.setNext(startNeighbor);
-				startNeighbor.setNext(null);				
-				
+				startNeighbor.setNext(null);
+
 				size++;
-				
-				
+
 				// case 3: new node is greater than or equal to neighbor of start ( or some
 				// subsequent neighbors of start)
 			} else {
-				
+
 				int index = 0;
-				int count = 0;	
+				int count = 0;
 
 				while (index == 0) {
 					if (getComp().compare(invoice, getNodeAtIndex(count).getInvoice()) >= 0) {
 						index = count;
 					}
 					count++;
-					
-				}				
-				
+
+				}
+
 				newInvoiceNode.setNext(getNodeAtIndex(index));
-				getNodeAtIndex(index-1).setNext(newInvoiceNode);
-							
-				
+				getNodeAtIndex(index - 1).setNext(newInvoiceNode);
+
 				size++;
-				
 
 			}
 
 		}
 
 	}
+	//Method to remove a node
+public boolean remove(int position) {
+		
+		if(position < 1 || position > size){
+			return false;
+		}
+		
+		InvoiceNode<Invoice> currentNode = start;
+		
+		
+		if (position == 1){
+			start = start.getNext();
+			size--;
+			return true;
+		}else if(position == size){
+			for(int i = 1; i < position - 1; i++){
+				if(currentNode.getNext() == null){
+					return false;
+				}
+				currentNode = currentNode.getNext();
+			}
+			currentNode.setNext(null);
+			size--;
+			return true;
+		}else{
+			InvoiceNode<Invoice> previousNode = start;
+			for(int i = 1; i < position-1; i++){
+				if(previousNode.getNext() == null){
+					return false;
+				}
+				previousNode = previousNode.getNext();
+			}
+			InvoiceNode<Invoice> nodeToBeRemoved = previousNode.getNext();
+			InvoiceNode<Invoice> nextNode = nodeToBeRemoved.getNext();
+			previousNode.setNext(nextNode);
+			size--;
+			return true;
+		}
+
+		
+	}
 	
 	
+
 }
